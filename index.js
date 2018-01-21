f796ed37d4a9640449cabc1105d856c163f5b19a
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-21 00:30:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {cake: 'cake mix'};
+
+ function updateObjectWithKeyAndValue(obj, key, value){
+  return Object.assign ({}, obj,{[key]: value});
+  
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+ obj[key] = value;
+ return obj
+}
+
+function deleteFromObjectByKey(obj, key){
+return delete obj.key;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj);
+  return delete newObj.key;
+}
+
+
+
+
