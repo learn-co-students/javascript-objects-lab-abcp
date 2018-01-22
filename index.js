91ca6679b8b5ec3405f1ad938b5bdfe7288fdaa8
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-22 02:19:17 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 function updateObjectWithKey(obj, key) {
 
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  delete newObj.key
+}
